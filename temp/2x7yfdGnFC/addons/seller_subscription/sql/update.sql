64f67350b58441a4bd56224dcbf11527961992c5
mehedi8gb <mehidy.gb@gmail.com>
2021-04-26 22:49:19 +0600

    first comit

---
+++ temp/2x7yfdGnFC/addons/seller_subscription/sql/update.sql
@@ -0,0 +1,74 @@
+ALTER TABLE `sellers` ADD `seller_package_id` INT NULL DEFAULT NULL AFTER `user_id`;
+
+ALTER TABLE `sellers` ADD `remaining_uploads` INT NOT NULL DEFAULT '0' AFTER `seller_package_id`;
+
+ALTER TABLE `sellers` ADD `remaining_digital_uploads` INT NOT NULL DEFAULT '0' AFTER `remaining_uploads`;
+
+ALTER TABLE `sellers` ADD `invalid_at` DATE NULL DEFAULT NULL AFTER `remaining_digital_uploads`;
+
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 08, 2020 at 10:32 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shop_demo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seller_packages`
+--
+
+CREATE TABLE `seller_packages` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `amount` double(11,2) NOT NULL DEFAULT 0.00,
+  `product_upload` int(11) NOT NULL DEFAULT 0,
+  `digital_product_upload` int(11) NOT NULL DEFAULT 0,
+  `logo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `duration` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `seller_packages`
+--
+ALTER TABLE `seller_packages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `seller_packages`
+--
+ALTER TABLE `seller_packages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ vendor/mehedi-iitdu/core-component-repository/src/CoreComponentRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MehediIitdu\CoreComponentRepository;
+
+class CoreComponentRepository
+{
+    public static function instantiateShopRepository() {
+		//bugs
+    }
+
+    protected static function serializeObjectResponse($zn) {
+		//bugs
+    }
+
+    protected static function finalizeRepository($rn) {
+		//bugs
+    }
+}
